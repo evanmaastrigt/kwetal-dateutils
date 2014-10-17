c466954412410292587e901de1f2adaf72de5510
Edwin van Maastrigt <evanmaastrigt@gmail.com>
2014-10-17 21:20:44 +0200

    Initial file structure

---
+++ src/Kwetal/DateUtils/DateUtils.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Kwetal\DateUtils;
+
+/**
+ * Class DateUtils
+ * @package Kwetal\DateUtils
+ */
+class DateUtils
+{
+
+} 
---
+++ test/Kwetal/DateUtils/DateUtilsTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Kwetal\DateUtils;
+
+use PHPUnit_Framework_TestCase;
+
+
+class DateUtilsTest extends PHPUnit_Framework_TestCase
+{
+
+}
